2630e244142509840dad8ba8fc9262d9741ea722
Johannes Gest <Johannes.Gest@haufe-lexware.com>
2016-11-25 16:06:08 +0100

    fix compile error

--- src/app/components/reservations/index.ts
+++ src/app/components/reservations/index.ts
@@ -1,5 +1,4 @@
 export * from './reservation.module';
 export * from './reservations.component';
-export * from './toggleable-reserved-item-list/toggleable-reserved-item-list.component';
 export * from './edit/edit-reservation.component';
 export * from './new/new-reservation.component';
