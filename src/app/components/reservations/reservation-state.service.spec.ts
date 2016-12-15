fa96a04953d0312d9ec5f33c0bb054473f19f323
Johannes Gest <Johannes.Gest@haufe-lexware.com>
2016-12-16 00:22:09 +0100

    bundled test cases

--- src/app/components/reservations/reservation-state.service.spec.ts
+++ src/app/components/reservations/reservation-state.service.spec.ts
@@ -65,78 +65,104 @@ describe('Service: ReservationState', () => {
         expect(reservationState.addedCount).toEqual(0);
     });
 
-    it('should store selected items', () => {
-        let selected: ItemStack[] = [];
-        selected.push(reservationState.stacks[0]);
-        selected.push(reservationState.stacks[2]);
+    describe('selected items', () => {
+        let selected: ItemStack[];
 
-        reservationState.select(selected);
+        beforeEach(() => {
+            selected = [];
+        });
 
-        expect(reservationState.stacks.length).toEqual(MOCKED_STACKS_SIZE);
-        expect(reservationState.selected.size).toEqual(2);
-        expect(reservationState.selected.has(selected[0])).toBe(true);
-        expect(reservationState.selected.has(selected[1])).toBe(true);
+        it('should store selected items', () => {
+            selected.push(reservationState.stacks[0]);
+            selected.push(reservationState.stacks[2]);
+
+            reservationState.select(selected);
+
+            expect(reservationState.stacks.length).toEqual(MOCKED_STACKS_SIZE);
+            expect(reservationState.selected.size).toEqual(2);
+            expect(reservationState.selected.has(selected[0])).toBe(true);
+            expect(reservationState.selected.has(selected[1])).toBe(true);
+        });
+
+        it('should modify stored selected items', () => {
+            selected.push(reservationState.stacks[0]);
+            selected.push(reservationState.stacks[1]);
+            selected.push(reservationState.stacks[2]);
+            selected.push(reservationState.stacks[3]);
+
+            let deselected: ItemStack[] = [];
+            deselected.push(reservationState.stacks[0]);
+            deselected.push(reservationState.stacks[2]);
+
+            reservationState.select(selected);
+            reservationState.deselect(deselected);
+
+            expect(reservationState.stacks.length).toEqual(MOCKED_STACKS_SIZE);
+            expect(reservationState.selected.size).toEqual(2);
+            expect(reservationState.selected.has(selected[1])).toBe(true);
+            expect(reservationState.selected.has(selected[3])).toBe(true);
+        });
+
+        it('should push stored selected items to reservation', () => {
+            selected.push(reservationState.stacks[0]);
+            selected.push(reservationState.stacks[2]);
+            selected[0].selectedCount = 1;
+            selected[1].selectedCount = 2;
+
+            reservationState.select(selected);
+            reservationState.pushSelectedToReservation();
+
+            expect(reservationState.stacks.length).toEqual(MOCKED_STACKS_SIZE);
+            expect(reservationState.selected.size).toEqual(0);
+            expect(reservationState.addedCount).toEqual(3);
+            expect(reservationState.added.length).toEqual(3);
+            expect(reservationState.added[0]).toBe(Array.from(selected[0].items)[0]);
+            expect(reservationState.added[1]).toBe(Array.from(selected[1].items)[0]);
+            expect(reservationState.added[2]).toBe(Array.from(selected[1].items)[1]);
+        });
+
     });
 
-    it('should modify stored selected items', () => {
-        let selected: ItemStack[] = [];
-        selected.push(reservationState.stacks[0]);
-        selected.push(reservationState.stacks[1]);
-        selected.push(reservationState.stacks[2]);
-        selected.push(reservationState.stacks[3]);
+    describe('blocked items', () => {
 
-        let deselected: ItemStack[] = [];
-        deselected.push(reservationState.stacks[0]);
-        deselected.push(reservationState.stacks[2]);
+        beforeEach(() => {
+            MOCKED_RESERVATIONS[0].begin = new Date(NOW - ONE_DAY);
+            MOCKED_RESERVATIONS[0].end = new Date(NOW + ONE_DAY);
+            MOCKED_RESERVATIONS[0].items = [
+                /*stack0 > 2 items*/items[0], items[1],
+                /*stack1* > 1 item*/items[6],
+                /*stack5* > all (5) items*/items[20], items[21], items[22], items[23]
+            ];
+        });
 
-        reservationState.select(selected);
-        reservationState.deselect(deselected);
+        it('should block when reservation ends after new begin', () => {
+            reservationState.reservation.begin = new Date(NOW);
 
-        expect(reservationState.stacks.length).toEqual(MOCKED_STACKS_SIZE);
-        expect(reservationState.selected.size).toEqual(2);
-        expect(reservationState.selected.has(selected[1])).toBe(true);
-        expect(reservationState.selected.has(selected[3])).toBe(true);
-    });
+            expect(reservationState.stacks[0].blockedCount).toEqual(2);
+            expect(reservationState.stacks[0].blocked).toBe(false);
+            expect(reservationState.stacks[1].blockedCount).toEqual(1);
+            expect(reservationState.stacks[1].blocked).toBe(false);
+            expect(reservationState.stacks[2].blockedCount).toEqual(0);
+            expect(reservationState.stacks[3].blockedCount).toEqual(0);
+            expect(reservationState.stacks[4].blockedCount).toEqual(0);
+            expect(reservationState.stacks[5].blockedCount).toEqual(4);
+            expect(reservationState.stacks[5].blocked).toBe(true);
+        });
 
-    it('should push stored selected items to reservation', () => {
-        let selected: ItemStack[] = [];
-        selected.push(reservationState.stacks[0]);
-        selected.push(reservationState.stacks[2]);
-        selected[0].selectedCount = 1;
-        selected[1].selectedCount = 2;
+        it('should block when reservation begins before new end', () => {
+            reservationState.reservation.end = new Date(NOW);
 
-        reservationState.select(selected);
-        reservationState.pushSelectedToReservation();
+            expect(reservationState.stacks[0].blockedCount).toEqual(2);
+            expect(reservationState.stacks[0].blocked).toBe(false);
+            expect(reservationState.stacks[1].blockedCount).toEqual(1);
+            expect(reservationState.stacks[1].blocked).toBe(false);
+            expect(reservationState.stacks[2].blockedCount).toEqual(0);
+            expect(reservationState.stacks[3].blockedCount).toEqual(0);
+            expect(reservationState.stacks[4].blockedCount).toEqual(0);
+            expect(reservationState.stacks[5].blockedCount).toEqual(4);
+            expect(reservationState.stacks[5].blocked).toBe(true);
+        });
 
-        expect(reservationState.stacks.length).toEqual(MOCKED_STACKS_SIZE);
-        expect(reservationState.selected.size).toEqual(0);
-        expect(reservationState.addedCount).toEqual(3);
-        expect(reservationState.added.length).toEqual(3);
-        expect(reservationState.added[0]).toBe(Array.from(selected[0].items)[0]);
-        expect(reservationState.added[1]).toBe(Array.from(selected[1].items)[0]);
-        expect(reservationState.added[2]).toBe(Array.from(selected[1].items)[1]);
-    });
-
-    it('should block items when reservation ends after new begin', () => {
-        MOCKED_RESERVATIONS[0].begin = new Date(NOW - ONE_DAY);
-        MOCKED_RESERVATIONS[0].end = new Date(NOW + ONE_DAY);
-        MOCKED_RESERVATIONS[0].items = [
-            /*stack0 > 2 items*/items[0], items[1],
-            /*stack1* > 1 item*/items[6],
-            /*stack5* > all (5) items*/items[20], items[21], items[22], items[23]
-        ];
-        reservationState.reservation.begin = new Date(NOW);
-        // reservationState.reservation.end = new Date(NOW + ONE_DAY);
-
-        expect(reservationState.stacks[0].blockedCount).toEqual(2);
-        expect(reservationState.stacks[0].blocked).toBe(false);
-        expect(reservationState.stacks[1].blockedCount).toEqual(1);
-        expect(reservationState.stacks[1].blocked).toBe(false);
-        expect(reservationState.stacks[2].blockedCount).toEqual(0);
-        expect(reservationState.stacks[3].blockedCount).toEqual(0);
-        expect(reservationState.stacks[4].blockedCount).toEqual(0);
-        expect(reservationState.stacks[5].blockedCount).toEqual(4);
-        expect(reservationState.stacks[5].blocked).toBe(true);
     });
 
 });
