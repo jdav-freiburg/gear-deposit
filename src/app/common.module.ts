ffc5524bcc942a0fe18c0f940371df9e59138e2a
Johannes Gest <Johannes.Gest@haufe-lexware.com>
2017-04-26 14:45:10 +0200

    optimized menu animation & removed unused code

--- src/app/common.module.ts
+++ src/app/common.module.ts
@@ -1,12 +1,10 @@
 import { CommonModule as AngularCommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DropdownDirective } from './attribute-directives';
 import { HeaderComponent } from './components/header/header.component';
 import { ItemStackComponent, SimpleItemListComponent } from './components/items';
 import { UiMessageComponent } from './components/ui-messages';
-import { GlobalClickDirective } from './directives';
-import { TransitionEndDirective } from './directives/transition-end.directive';
+import { GlobalClickDirective, TransitionEndDirective } from './directives';
 import { ItemFilterPipe } from './pipes';
 import { ItemService, LoadingService, UiMessageService } from './services';
 
@@ -21,7 +19,6 @@ import { ItemService, LoadingService, UiMessageService } from './services';
         ItemStackComponent,
         ItemFilterPipe,
         UiMessageComponent,
-        DropdownDirective,
         GlobalClickDirective,
         TransitionEndDirective
     ],
@@ -40,7 +37,6 @@ import { ItemService, LoadingService, UiMessageService } from './services';
         ItemStackComponent,
         ItemFilterPipe,
         UiMessageComponent,
-        DropdownDirective,
         GlobalClickDirective,
         TransitionEndDirective
     ]
--- src/app/directives/index.ts
+++ src/app/directives/index.ts
@@ -1,1 +1,3 @@
 export * from './global-click.directive';
+export * from './transition-end.directive';
+export * from './update-min-height-to-viewport.directive';
